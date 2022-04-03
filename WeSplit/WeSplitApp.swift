27dbfbc01130afa4acfb6823f9ec29013820fdc9
Shokri <shokre4@hotmail.com>
2022-04-03 21:19:29 +0400

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Shokri Alnajjar on 03/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
